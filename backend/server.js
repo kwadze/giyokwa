63162b2f23e846f6b59f621c5329798aaf394a78
kwaclovis <kwaclovis@lead.ac.in>
2020-08-28 02:30:46 -0700

    third commit

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import data from './data';
 
 import config from './config';
 import mongoose from 'mongoose';
+
 import bodyParser from 'body-parser';
 import userRoute from './routes/userRoute';
 import productRoute from './routes/productRoute';
@@ -40,4 +41,4 @@ app.get("/api/config/paypal", (req, res) => {
 //   res.send(data.products);
 // });
 
-app.listen(5000, () => { console.log("Server started at http://localhost:5000") });
+app.listen(process.env.PORT ||5000, () => { console.log("Server started succefully") });
